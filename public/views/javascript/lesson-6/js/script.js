b539ee178ee683efb0da62131dc1974890b65f00
serhioses <serega.mirosh@gmail.com>
2018-06-01 23:21:20 +0300

    js lesson 6 hw

--- public/views/javascript/lesson-6/js/script.js
+++ public/views/javascript/lesson-6/js/script.js
@@ -243,3 +243,44 @@
 // let cat2 = cloneObj(cat);
 // console.log(cat2);
 // console.log(cat2 === cat);
+
+// ADDITIONAL TASKS
+// Reverse the given string
+(function () {
+  function funcName (param) {
+    // code...
+  }
+  funcName('Some string'); // -> 'gnirts emoS'
+}());
+
+// Compare two objects
+// A function must return true if two objects refer to the same object or they refer to different objects but have the same keys number and their corresponding values are equal. The function must work with unlimited nestend objects and must take them into account if needed
+(function () {
+  function funcName (param1, param2, param3) {
+    // code...
+  }
+  funcName({key: 'Value'}, {key: 'Value'}, true); // -> true
+  funcName({key: 'Value'}, {key: 'Value 1'}, true); // -> false
+  funcName({key: 'Value', nested: {key: 'Value'}}, {key: 'Value 1', nested: {key: 'Value'}}, true); // -> true
+  funcName({key: 'Value', nested: {key: 'Value'}}, {key: 'Value 1', nested: {key: 'Value'}}); // -> false
+}());
+
+// Draw an area
+// Draw an area N wide and N height like it the example below. Use ' @ ' and ' - '. The numbers before a line start are necessary to prevent console from joining the same data logged.
+(function () {
+  function funcName (N) {
+    // code...
+  }
+  funcName(8); // -> http://prntscr.com/jpobhn
+}());
+
+// Strings comparison
+// Check whether two strings have the same set of chars. Return true if they are equal length and have the same set of chars no matter at which index those chars are
+(function () {
+  function funcName (param1, param2) {
+    // code...
+  }
+  funcName('clock', 'lockc'); // -> true. Because two "c", one "l", one "o" and one "k". Doesn't matter they are at different positions
+  funcName('tree', 'three'); // -> false. Different lenghts
+  funcName('mttudd', 'dutmtd'); // -> true. Because two "t", two "d", one "m" and one "u". Doesn't matter they are at different positions
+}());
