6ba601f26c4348be3cbc4f1feeec0bab01fa0a77
serhioses <serega.mirosh@gmail.com>
2018-07-30 22:50:08 +0300

    js lesson 20 hw

---
+++ public/views/javascript/lesson-21/js/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// Element metrics and scroll
+// always number in pixels
+// for "invisible" elements 0x0
+// const metrics = document.querySelector('.metrics');
+// const computedStyle = getComputedStyle(metrics);
+// offsetParent
+// console.log(metrics.offsetParent);
+// offsetTop, offsetLeft
+// console.log('top:', metrics.offsetTop);
+// console.log('left:', metrics.offsetLeft);
+
+// offsetWidth, offsetHeight
+// console.log('full width with the border:', metrics.offsetWidth);
+// console.log('full height with the border:', metrics.offsetHeight);
+
+// clientTop, clientLeft
+// distance between the outer area and the inner area of an element including scroll bar
+// console.log('clientTop:', metrics.clientTop);
+// console.log('clientLeft:', metrics.clientLeft);
+// console.log(metrics.offsetWidth - metrics.clientLeft - metrics.clientWidth);
+
+// clientWidth, clientHeight
+// content size + paddings - scroll bar width (depends on a browser)
+// console.log('clientWidth', metrics.clientWidth);
+// console.log('clientHeight', metrics.clientHeight);
+// console.log('computed width:', computedStyle.width);
+// console.log('offset width:', metrics.offsetWidth);
+// console.log('client width:', metrics.clientWidth);
+
+// scrollWidth, scrollHeight
+// console.log('scroll width:', metrics.scrollWidth);
+// console.log('scroll height:', metrics.scrollHeight);
+
+// scrollTop, scrollLeft
+// console.log('scroll top:', metrics.scrollTop);
+// console.log('scroll left:', metrics.scrollLeft);
+// metrics.addEventListener('scroll', function () {
+//   console.log('scroll top:', metrics.scrollTop);
+// });
